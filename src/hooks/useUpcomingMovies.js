019b50779b0b054cc7fb2dc86ccd354ece99fb72
bmbx08 <bmbx08@bu.ac.kr>
2024-08-02 17:53:28 +0900

    added upcoming movies section

---
+++ src/hooks/useUpcomingMovies.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import api from '../utils/api';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchUpcomingMovies = () => {
+    return api.get('/movie/upcoming ');
+};
+
+export const useUpcomingMoviesQuery=()=>{
+    return useQuery({
+        queryKey:["movie-upcoming"],
+        queryFn: fetchUpcomingMovies,
+        select:(result)=>result.data
+    });
+};
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Banner from './components/Banner/Banner'
+import UpcomingMovieSlide from './components/UpcomingMovieSlide/UpcomingMovieSlide'
 import PopularMovieSlide from './components/PopularMovieSlide/PopularMovieSlide'
 
 //1. 배너 -> popular영화 중 첫 번째 아이템 보여주기
@@ -12,6 +13,7 @@ const Homepage = () => {
     <div>
       <Banner/>
       <PopularMovieSlide/>
+      <UpcomingMovieSlide/>
     </div>
   )
 }
