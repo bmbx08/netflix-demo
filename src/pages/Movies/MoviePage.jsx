c9de160da2fa1afbd9341959cc7a0350e875fb16
bmbx08 <bmbx08@bu.ac.kr>
2024-08-09 06:49:11 +0900

    added card css, videos in detail section

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -6,6 +6,9 @@ import MovieCard from "../../common/MovieCard/MovieCard";
 import ReactPaginate from "react-paginate";
 import LoadingSpinner from "../../common/Loading/LoadingSpinner";
 import ErrorMessage from "../../common/Loading/ErrorMessage";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
 
 //경로 2가지
 //1.nav바에서 클랙해서 온 경우 -> popuarMovie 보여주기
@@ -28,6 +31,32 @@ const MoviePage = () => {
     setPage(selected+1); //질문: page state가 바뀔때마다 api호출이 왜 되는지?
   };
 
+  const sortByPopularIncrease=()=>{
+    data.results.sort(function(a,b){
+      if(a.popularity<b.popularity){
+        return 1;
+      }
+      if(a.popularity>b.popularity){
+        return -1;
+      }
+      return 0;
+    })
+    console.log("inc popularity data",data.results)
+  }
+
+  const sortByPopularDecrease=()=>{
+    data.results.sort(function(a,b){
+      if(a.popularity>b.popularity){
+        return 1;
+      }
+      if(a.popularity<b.popularity){
+        return -1;
+      }
+      return 0;
+    })
+    console.log("dec popularity data",data.results)
+  }
+
   if (isLoading) {
     return <LoadingSpinner version="version1"/>;
   }
@@ -39,7 +68,11 @@ const MoviePage = () => {
     <Container>
       <Row>
         <Col lg={4} xs={12}>
-          필터
+          {/* 필터 */}
+          <DropdownButton id="dropdown-basic-button" title="Sort By..">
+            <Dropdown.Item onClick={sortByPopularDecrease}>Most Popular</Dropdown.Item>
+            <Dropdown.Item onClick={sortByPopularIncrease}>Least Popular</Dropdown.Item>
+          </DropdownButton>
         </Col>
         <Col lg={8} xs={12}>
           <Row>
